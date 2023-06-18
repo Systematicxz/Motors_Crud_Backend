3be45d2138510562c6540955850b52cdb84cd9db
Systematicxz <tostado_mt@hotmail.com>
2023-06-18 14:23:27 -0600

    v2.1

--- src/database/config.js
+++ src/database/config.js
@@ -1,17 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize({
-  // dialect: process.env.DB_DIALECT,
-  // host: process.env.DB_HOST,
-  // username: process.env.DB_USERMAME,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE,
-  // logging: false,
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'root',
-  database: 'motors',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERMAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   logging: false,
 });
 
